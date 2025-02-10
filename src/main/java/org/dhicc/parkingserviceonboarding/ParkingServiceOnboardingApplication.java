6f11a406f238448910d92131ee3a4492eca4ea30
김희석 <hglad0216@gmail.com>
2025-02-10 15:41:26 +0900

    git init

---
+++ src/main/java/org/dhicc/parkingserviceonboarding/ParkingServiceOnboardingApplication.java
@@ -0,0 +1,13 @@
+package org.dhicc.parkingserviceonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingServiceOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingServiceOnboardingApplication.class, args);
+    }
+
+}
