870428299b3d8aae71f263862216831e4390bfce
김희석 <hglad0216@gmail.com>
2025-02-11 13:31:38 +0900

    feat : 예외처리 기능 추가

---
+++ src/main/java/org/dhicc/parkingserviceonboarding/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package org.dhicc.parkingserviceonboarding.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
+        return errorResponse;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleGlobalException(Exception ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "서버 내부 오류가 발생했습니다.");
+        return errorResponse;
+    }
+}
