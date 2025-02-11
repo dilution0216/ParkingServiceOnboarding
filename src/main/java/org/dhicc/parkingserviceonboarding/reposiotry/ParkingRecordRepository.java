63b30ec30d37a2aaa12d6f2efe6aad03ee2079db
김희석 <hglad0216@gmail.com>
2025-02-11 13:41:22 +0900

    feat : 차량 번호별 주차 기록 조회 기능 추가

--- src/main/java/org/dhicc/parkingserviceonboarding/reposiotry/ParkingRecordRepository.java
+++ src/main/java/org/dhicc/parkingserviceonboarding/reposiotry/ParkingRecordRepository.java
@@ -4,8 +4,12 @@ import org.dhicc.parkingserviceonboarding.model.ParkingRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ParkingRecordRepository extends JpaRepository<ParkingRecord, Long> {
     ParkingRecord findByVehicleNumberAndExitTimeIsNull(String vehicleNumber);
     ParkingRecord findByVehicleNumberAndExitTimeIsNotNull(String vehicleNumber);
+    // 차량 번호별 주차 기록 조회
+    List<ParkingRecord> findByVehicleNumber(String vehicleNumber);
 }
